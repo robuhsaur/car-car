6379e26fbdd9030925f99d081320059f7e54d23f
Robert Nacario <r.nacariojr24@gmail.com>
2023-02-11 23:33:39 -0800

    images added

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -16,35 +16,39 @@ import { ServiceAppointmentForm } from './ServiceForm';
 import TechnicianForm from './TechnicianForm'
 import AppointmentsList from './ServiceList'
 import ServiceHistory from './ServiceHistory'
+import gr86 from "./img/gr86.jpg"
+
 
 
 function App(props) {
   return (
     <BrowserRouter>
-      <Nav />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/models/create" element={<CreateVehicleModel />} />
-          <Route path="/automobiles" element={<ListAutomobiles />} />
-          <Route path="/manufacturers" element={<ManufacturerList manufacturers={props.manufacturers} />} />
-          <Route path="/manufacturers/create" element={<ManufacturerForm />} />
-          <Route path="/models" element={<VehicleModelList />} />
-          <Route path="/automobiles/create" element={<AutomobileForm />} />
-          <Route path="/customers/create" element={<CreateCustomer />} />
-          <Route path="/salesperson/create" element={<CreateSalesPerson />} />
-          <Route path="/salesrecord" element={<SalesRecordListAll />} />
-          <Route path="/salesrecord/create" element={<CreateSalesRecord />} />
-          <Route path="/salesrecord/history" element={<SalespersonHistory />} />
-        </Routes>
-        <Routes >
-          <Route path="/service" element={<AppointmentsList appointment={props.appointment} />} />
-          <Route path="/service/new" element={<ServiceAppointmentForm />} />
-          <Route path="/service/history" element={<ServiceHistory />} />
-        </Routes>
-        <Routes>
-          <Route path="/technician/new" element={<TechnicianForm />} />
-        </Routes>
+      <div style={{ backgroundImage: `url(${gr86})` }}>
+        <Nav />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/models/create" element={<CreateVehicleModel />} />
+            <Route path="/automobiles" element={<ListAutomobiles />} />
+            <Route path="/manufacturers" element={<ManufacturerList manufacturers={props.manufacturers} />} />
+            <Route path="/manufacturers/create" element={<ManufacturerForm />} />
+            <Route path="/models" element={<VehicleModelList />} />
+            <Route path="/automobiles/create" element={<AutomobileForm />} />
+            <Route path="/customers/create" element={<CreateCustomer />} />
+            <Route path="/salesperson/create" element={<CreateSalesPerson />} />
+            <Route path="/salesrecord" element={<SalesRecordListAll />} />
+            <Route path="/salesrecord/create" element={<CreateSalesRecord />} />
+            <Route path="/salesrecord/history" element={<SalespersonHistory />} />
+          </Routes>
+          <Routes >
+            <Route path="/service" element={<AppointmentsList appointment={props.appointment} />} />
+            <Route path="/service/new" element={<ServiceAppointmentForm />} />
+            <Route path="/service/history" element={<ServiceHistory />} />
+          </Routes>
+          <Routes>
+            <Route path="/technician/new" element={<TechnicianForm />} />
+          </Routes>
+        </div>
       </div>
     </BrowserRouter>
   );
